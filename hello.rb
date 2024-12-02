21bfa45e08303bbad527edf097ef0ae3cefa5cb1
arthur <arthur.broudoux@gmail.com>
2024-12-02 22:13:53 +0100

    fix: rename repository name (hello -> homebrew-hello) Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- hello.rb
+++ hello.rb
@@ -1,7 +1,7 @@
 class Pm < Formula
     desc "Hello world!"
-    homepage "https://github.com/abroudoux/hello"
-    url "https://github.com/abroudoux/hello/archive/1.0.0.tar.gz"
+    homepage "https://github.com/abroudoux/homebrew-hello"
+    url "https://github.com/abroudoux/homebrew-hello/archive/1.0.0.tar.gz"
     sha256 "aa7536ef9061c1ed270140bc8baf9d7dcf3cdeff"
     version "1.0.0"
 
